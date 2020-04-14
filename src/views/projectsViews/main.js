dcc27d045fee9c3f8be65fe392d014b49816d00b
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-04-14 16:53:07 -0500

    finish styles in mainView. Define html for todo lists

---
+++ src/views/projectsViews/main.js
@@ -0,0 +1,30 @@
+export const mainView = ` <main>
+<aside>
+    <div class="index">
+        <div id="aside-active"><img src="../src/assets/images/to-do-list.svg" alt="list icon">Boards</div>
+        <div><img src="../src/assets/images/to-do-list.svg" alt="list icon">plantillas</div>
+        <div><img src="../src/assets/images/to-do-list.svg" alt="list icon">inicio</div>
+    </div>
+    <div class="teams">
+        <h2>TEAMS</h2>
+        <button><img src="../src/assets/images/plus.svg" alt="list icon">Create a Team</button>
+    </div>
+</aside>
+<div class="boards">
+    <h1 id="personal-board"><img src="../src/assets/images/user.svg" alt="user icon">Personal Boards</h1>
+
+    <div class="my-boards">
+        <div class="tests">Learn maths with python</div>
+        <div class="tests">Learn maths with python</div>
+        <div class="tests">Learn maths with python</div>
+        <div class="tests">Learn maths with python</div>
+        <div class="tests">Learn maths with python</div>
+        <div class="tests">Learn maths with python</div>
+        <div class="tests">Learn maths with python</div>
+        <div class="tests">Learn maths with python</div>
+        <div class="tests">Learn maths with python</div>
+        <div class="tests">Learn maths with python</div>
+        <a class="add-board">Create a new board</a>
+    </div>
+</div>
+</main>`
