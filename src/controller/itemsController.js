bc3e07aa7c8bf29487e4876d9cb0628faff43bba
Bruna <brunagenz92@gmail.com>
2020-04-17 19:59:39 -0300

    refactor code

--- src/controller/itemsController.js
+++ src/controller/itemsController.js
@@ -2,28 +2,26 @@ import { optionsView } from "../views/listsViews/optionsView";
 import { closeForm } from './projectsController.js';
 import { insertPage } from "../routes/routesConfig";
 
+const itemState = {}
+
 const root = document.querySelector('.root')
-
-
-
 
 // 1) Add event listener to Add Item button
 
 const actionToInput = (input,event,item) => {
     input.addEventListener(event, ()=> {
-        return item = input.value
-        
+        itemState[item] = input.value
     })
 }
 
 const insertItem = (name) => {
-    `<div class="items">${name}</div>`
+    return `<div class="items">${name}</div>`
 }
 root.addEventListener('click', (e)=> {
     if (e.target.matches('.add-item, .add-item *')) {
         
         // Save list ID and name
-        const parentList = e.target.closest('.list')
+        const parentListID = e.target.closest('.list').dataset.listid
         const parentListName = e.target.closest('.list').dataset.listtitle
         
         // 2) Pop up item form
@@ -31,24 +29,28 @@ root.addEventListener('click', (e)=> {
         closeForm('#item-options, #item-options *')
 
         // 3) Get values from inputs
-        let itemName
+        //let itemName
         const nameInput = document.querySelector('#list-name-input')
-        actionToInput(nameInput,"keydown",itemName)
+        itemState.name = actionToInput(nameInput,"keydown", "name")
         
-        let itemDescription
+        //let itemDescription
         const descriptionInput = document.querySelector('#description')
-        actionToInput(descriptionInput,"keydown",itemDescription)
+        itemState.description = actionToInput(descriptionInput,"keydown","description")
 
-        let itemPriority
+        //let itemPriority
         const priorityInput = document.querySelector('#priority')
-        actionToInput(priorityInput,"click",itemPriority)
+        itemState.priority = actionToInput(priorityInput,"click","priority")
         
         // 4) Create item object when click save button
 
+        //! we need to figure out how to put the item in the correct place
+
         const saveBtn = document.querySelector('#save-btn')
+        //const itemsContainer = e.target.previousElementSibling.previousElementSibling
+        console.log(itemsContainer)
 
         saveBtn.addEventListener('click', ()=> {
-            parentList.insertAdjacentHTML('beforeend',insertPage())
+            itemsContainer.insertAdjacentHTML('afterbegin', insertItem(itemState.name))
             
         })
     }
