1b3954f56b3091e1e0f45f673184eee83379aee0
Bruna <brunagenz92@gmail.com>
2020-06-17 19:05:25 -0300

    remove non-functional buttons on boards and lists views

--- src/views/projectsViews/navView.js
+++ src/views/projectsViews/navView.js
@@ -1,22 +1,13 @@
 export default `
 <nav>
     <div>
-    
-
-        <div><img src="../src/assets/images/grid.svg" alt="menu icon" class="nav-icon"></div>
         <div onClick="window.location.reload()" class="home-link"><img src="../src/assets/images/home-run.svg"  alt="home icon"></div>
-        <div onClick="window.location.reload()" class="home-link"><img src="../src/assets/images/to-do-list.svg" alt="list icon"><p>Boards</p></div> 
-        <div><input type="text"><img src="../src/assets/images/search.svg" alt="search icon"></div>
-    
+        <div onClick="window.location.reload()" class="home-link"><img src="../src/assets/images/to-do-list.svg" alt="list icon"><p>Boards</p></div>  
     </div>
     <div>
         <div id="logo"><img src="../src/assets/images/to-do-list.svg" alt="list icon"><h1>SuperToDo</h1></div>
     </div>
     <div>
-        <div><img src="../src/assets/images/plus.svg" alt="plus icon"></div>
-        <div><img src="../src/assets/images/information.svg" alt="information icon"></div>
-        <div><img src="../src/assets/images/bell.svg" alt="bell icon"></div>
-        <div><img src="../src/assets/images/user.svg" alt="user icon"></div>
     </div>
 </nav>
 `;
